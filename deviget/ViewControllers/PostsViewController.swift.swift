cc78de19a7813b293898f7cefc6af31201ef06d4
Christian Quicano <ceo@christianquicano.pro>
2019-02-16 11:36:25 -0500

    Created paginator, load more

--- deviget/ViewControllers/PostsViewController.swift.swift
+++ deviget/ViewControllers/PostsViewController.swift.swift
@@ -14,12 +14,17 @@ class PostsViewController: UITableViewController {
     //MARK:- privates properties
     private var posts = [EntryReddit]()
     private var afterPostString:String?
+    private let sectionPosts    = 0
+    private let sectionLoadMore = 1
+    private var isLoadingPosts = false
     weak private var presenter:PostsPresenterDelegate?
 
     //MARK:- UIViewController
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.tableFooterView = UIView()
         PostsConfigurator.configure(view: self)//create MVP
+        isLoadingPosts = true
         presenter?.loadPosts()
     }
 
@@ -29,25 +34,51 @@ class PostsViewController: UITableViewController {
 
     // MARK: - Table View
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return 2
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return posts.count
+        return section == sectionLoadMore ? 1 : posts.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let section = indexPath.section
+        let row = indexPath.row
+        
+        if section == sectionLoadMore {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "loadingCell", for: indexPath)
+            let activityIndicator = cell.viewWithTag(1) as? UIActivityIndicatorView
+            activityIndicator?.startAnimating()
+            return cell
+        }
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let post = posts[indexPath.row]
+        let post = posts[row]
         cell.textLabel?.text = post.title
         return cell
     }
 
+    override func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        guard !isLoadingPosts else { return }
+        //validate if is the last row
+        let visibleIndex = tableView.indexPathsForVisibleRows ?? []
+        let total = posts.count - 1
+        let contain = visibleIndex.contains { (indexPath) -> Bool in
+            indexPath.row >= total ? true : false
+        }
+        if contain {
+            PostsConfigurator.setNewURL(Util.createPostsUrl(after: afterPostString))
+            isLoadingPosts = true
+            presenter?.loadPosts()
+        }
+    }
+    
 }
 // MARK: - PostsViewDelegate
 extension PostsViewController:PostsViewDelegate {
     
     func showEntries(_ entries: [EntryReddit]) {
+        isLoadingPosts = false
         posts.append(contentsOf: entries)
         tableView.reloadData()
     }
@@ -61,7 +92,7 @@ extension PostsViewController:PostsViewDelegate {
     }
     
     func showNetworkError(_ error: Error?) {
-        
+        isLoadingPosts = false
     }
     
 }
