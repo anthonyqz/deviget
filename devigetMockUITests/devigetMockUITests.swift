fcb113f532d697a9f923289b29736b90907d0b83
Christian Quicano <ceo@christianquicano.pro>
2019-02-17 11:19:22 -0500

    created UITest - coverage 84.7%

---
+++ devigetMockUITests/devigetMockUITests.swift
@@ -0,0 +1,47 @@
+//
+//  devigetMockUITests.swift
+//  devigetMockUITests
+//
+//  Created by Christian Quicano on 2/17/19.
+//  Copyright © 2019 christianquicano. All rights reserved.
+//
+
+import XCTest
+
+class devigetMockUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testDismissPosts() {
+        sleep(1)
+        
+        let authorPost = "t2_eg0d02d"
+        let dimissButtonTitle = "Dismiss Post"
+        
+        let app = XCUIApplication()
+        let tablesQuery = app.tables
+        let cell = tablesQuery.cells.containing(.staticText, identifier: authorPost)
+        let titleAuthor = cell.staticTexts[authorPost]
+        XCTAssertTrue(titleAuthor.exists)
+        let dismissButton = cell.buttons[dimissButtonTitle]
+        XCTAssertNotNil(dismissButton)
+        dismissButton.tap()
+        let titleAuthorNil = tablesQuery.cells.staticTexts[authorPost]
+        XCTAssertFalse(titleAuthorNil.exists)
+    }
+
+}
